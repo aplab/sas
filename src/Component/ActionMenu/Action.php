f983e546061c27f2f74c83f71af1861954ec61cd
polyanin <polyanin@gmail.com>
2022-02-07 14:21:32 +0500

    upgrade framework version

--- src/Component/ActionMenu/Action.php
+++ src/Component/ActionMenu/Action.php
@@ -1,15 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: polyanin
- * Date: 14.08.2018
- * Time: 14:50
- */
-
-namespace App\Component\ActionMenu;
-
+<?php namespace App\Component\ActionMenu;
 
 abstract class Action
 {
-
 }
