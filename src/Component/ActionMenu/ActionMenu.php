419c6e9f3d1a500ebcb320302a7e6eee71346e80
polyanin <polyanin@gmail.com>
2022-02-08 02:09:28 +0500

    refactoring 20220208020927

--- src/Component/ActionMenu/ActionMenu.php
+++ src/Component/ActionMenu/ActionMenu.php
@@ -1,106 +1,61 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: polyanin
- * Date: 22.08.2018
- * Time: 15:20
- */
+<?php namespace App\Component\ActionMenu;
 
-namespace App\Component\ActionMenu;
-
+use JetBrains\PhpStorm\ArrayShape;
 use JsonSerializable;
 
-/**
- * Class ActionMenu
- * @package App\Component\ActionMenu
- */
 class ActionMenu implements JsonSerializable
 {
-    /**
-     * @var static[]
-     */
-    protected static $instances = [];
+    /* @var static[] */
+    protected static array $instances = [];
+    protected string $id;
 
-    /**
-     * @var string
-     */
-    protected $id;
-
-    /**
-     * @return string
-     */
-    public function getId()
+    public function getId(): string
     {
         return $this->id;
     }
 
-    /**
-     * @param string $id
-     * @return static
-     * @throws Exception
-     */
-    public function setId(string $id)
+    /* @throws Exception */
+    public function setId(string $id): static
     {
         $this->id = $id;
         static::registerInstance($this);
         return $this;
     }
 
-    /**
-     * @var MenuItem[]
-     */
-    protected $items = [];
+    /* @var MenuItem[] */
+    protected array $items = [];
 
-    /**
-     * @return MenuItem[]
-     */
-    public function getItems()
+    /* @return MenuItem[] */
+    public function getItems(): array
     {
         return $this->items;
     }
 
-    /**
-     * @param MenuItem $item
-     * @return ActionMenu
-     */
-    public function addItem(MenuItem $item)
+    public function addItem(MenuItem $item): static
     {
         $this->items[] = $item;
         return $this;
     }
 
-    /**
-     * MainMenu constructor.
-     * @param string $id
-     * @throws Exception
-     */
+    /* @throws Exception */
     public function __construct(string $id)
     {
         $this->id = $id;
         static::registerInstance($this);
     }
 
-    /**
-     * @throws Exception
-     */
+    /* @throws Exception */
     public function __wakeup()
     {
         static::registerInstance($this);
     }
 
-    /**
-     * @param string $id
-     * @return ActionMenu|null
-     */
-    public static function getInstance(string $id)
+    public static function getInstance(string $id): ?ActionMenu
     {
         return static::$instances[$id] ?? null;
     }
 
-    /**
-     * @param self $instance
-     * @throws Exception
-     */
+    /* @throws Exception */
     private static function registerInstance(ActionMenu $instance)
     {
         $id = $instance->getId();
@@ -110,36 +65,23 @@ class ActionMenu implements JsonSerializable
         static::$instances[$id] = $instance;
     }
 
-    /**
-     * Specify data which should be serialized to JSON
-     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     * which is a value of any type other than a resource.
-     * @since 5.4.0
-     */
-    public function jsonSerialize()
+    #[ArrayShape(['id' => "string", 'items' => "array|array[]"])]
+    public function jsonSerialize(): array
     {
         return [
             'id' => $this->id,
-            'items' => array_map(function(MenuItem $i) {
+            'items' => array_map(function (MenuItem $i) {
                 return $i->jsonSerialize();
             }, $this->items)
         ];
     }
 
-    /**
-     * @return string
-     */
-    public function __toString()
+    public function __toString(): string
     {
         return json_encode($this);
     }
 
-    /**
-     * @param int $options
-     * @return string
-     */
-    public function __toJson($options = JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE)
+    public function __toJson(int|string $options = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE): string
     {
         return json_encode($this, $options);
     }
